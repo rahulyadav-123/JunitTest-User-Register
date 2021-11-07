abefc6808ff6c29d9921d9c519a5f6a2834f08a2
rahul yadav <ry219613@gmail.com>
2021-11-07 22:12:03 +0530

    Added unit testing for first name

--- com/labz/test/UserRegisterTest.java
+++ com/labz/test/UserRegisterTest.java
@@ -14,33 +14,7 @@ public class UserRegisterTest {
 		Assert.assertEquals("Brijesh", actual);
 	}
 	
-	@Test
-	public void givenLastName_isProper() {
-		UserRegister register = new UserRegister();
-		String actual = register.lastName("Kulal");
-		Assert.assertEquals("Kulal", actual);
-	}
 	
-	@Test
-	public void givenEmailId_isProper() {
-		UserRegister register = new UserRegister();
-		String actual = register.emailId("brijeshkulal534@gmail.com");
-		Assert.assertEquals("brijeshkulal534@gmail.com", actual);
-	}
-	
-	@Test
-	public void givenPhoneNumber_isProper() {
-		UserRegister register = new UserRegister();
-		String actual = register.phoneNumber("8289830020");
-		Assert.assertEquals("8289830020", actual);
-	}
-	
-	@Test
-	public void givenPassword_isProper() {
-		UserRegister register = new UserRegister();
-		String actual = register.password("vAvc3@ttaay");
-		Assert.assertEquals("vAvc3@ttaay", actual);
-	}
 }
 
 
