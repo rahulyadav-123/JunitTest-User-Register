fcae63c8b8794e664608707586ef31b61f1680f9
rahulyadav-123 <90572209+rahulyadav-123@users.noreply.github.com>
2021-11-07 22:34:12 +0530

    Added unit testing Last Name

--- com/labz/test/UserRegisterTest.java
+++ com/labz/test/UserRegisterTest.java
@@ -16,8 +16,8 @@ public class UserRegisterTest {
 	@Test
 	public void givenLastName_isProper() {
 		UserRegister register = new UserRegister();
-		String actual = register.LastName("Rahul");
-		Assert.assertEquals("Rahul", actual);
+		String actual = register.LastName("Yadav");
+		Assert.assertEquals("Yadav", actual);
 	}
 	
 	
